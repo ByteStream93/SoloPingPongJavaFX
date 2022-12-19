c5ca4a2858fa67bf97cd363ee5d259521ea38ca4
ByteStream93 <116450794+ByteStream93@users.noreply.github.com>
2022-12-19 12:52:29 +0100

    Update GameTick.java

--- src/main/java/com/example/pingpong/GameTick.java
+++ src/main/java/com/example/pingpong/GameTick.java
@@ -3,6 +3,7 @@ package com.example.pingpong;
 public class GameTick {
 
     public static void ticking(){
+        
         Table.drawTable(Main.graphicsContext);
         Bar.drawBar(Main.graphicsContext);
         Ball.drawBall(Main.graphicsContext);
