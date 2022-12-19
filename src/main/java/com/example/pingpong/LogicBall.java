de89f7fda7927f2083406ab13296e96cb66a6f7a
ByteStream93 <116450794+ByteStream93@users.noreply.github.com>
2022-12-19 12:53:15 +0100

    Update LogicBall.java

--- src/main/java/com/example/pingpong/LogicBall.java
+++ src/main/java/com/example/pingpong/LogicBall.java
@@ -10,6 +10,7 @@ public class LogicBall {
     public static void hitBar() {
 
         for (int i = 0; i < Main.gameBar.size(); i++) {
+            
             if (Main.gameBar.get(i).getX_coordinate() + 1 == Main.ball.getX_coordinate() && Main.gameBar.get(i).y_coordinate == Main.ball.y_coordinate) {
                 BallDirection.setMoving_x(1);
 
@@ -27,11 +28,13 @@ public class LogicBall {
                 }
                 HighScore.score++;
             }
+            
             if(Main.gameBar.get(i).getX_coordinate()==Main.ball.getX_coordinate()&&Main.gameBar.get(i).getY_coordinate()+1==Main.ball.getY_coordinate()){
                 BallDirection.setMoving_x(1);
                 BallDirection.setMoving_y(3);
                 HighScore.score++;
             }
+            
             if(Main.gameBar.get(i).getX_coordinate()==Main.ball.getX_coordinate()&&Main.gameBar.get(i).getY_coordinate()-1==Main.ball.getY_coordinate()){
                 BallDirection.setMoving_x(1);
                 BallDirection.setMoving_y(-3);
@@ -41,12 +44,15 @@ public class LogicBall {
         }
     }
         public static void checkBorder(){
+            
             if(Main.ball.y_coordinate<=0 || Main.ball.y_coordinate>=Table.rows-3){
                 BallDirection.setMoving_y(BallDirection.getMoving_y()*(-1));
             }
+            
             if(Main.ball.x_coordinate==0){
                 System.exit(0);
         }
+            
             if(Main.ball.x_coordinate==Table.columns-2){
                 BallDirection.setMoving_x(BallDirection.getMoving_x()*(-1));
             }
